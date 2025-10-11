1b6566c17f8cd98e39e230a3b50cc7be3980deb5
atoor0807 <atoor0807@gmail.com>
2025-10-11 15:51:01 +0500

    meow

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import Hello from './Hello'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    
+    <h3>
+      Hey wassup
+    </h3>
+    < Hello/>
     </>
   )
 }
