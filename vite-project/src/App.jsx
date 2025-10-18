8373e6e622f11f1714e1775a0db16f6bf7334bdd
atoor0807 <atoor0807@gmail.com>
2025-10-18 23:29:14 +0500

    make a simple counter app to learn hooks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 import Hello from './Hello'
+import React from 'react'
 import './App.css'
 
+
 function App() {
+  let [counter, setCounter] = useState(20)  
+  let increasenumber = ()=>{
+    if(counter==20){return }
+    setCounter(counter+1)
+  }
+
+  let decreasevalue=()=>{
+    if(counter==0){return}
+    setCounter(counter-1)
+  }
 
   return (
     <>
-    <h3>
-      Hey wassup
-    </h3>
-    < Hello/>
+    
+    <h1>
+      Counter App
+    </h1>
+    <p> 
+      Value : {counter}
+    </p>
+    <button onClick={increasenumber}>
+      increase value
+    </button>
+    <button onClick={decreasevalue}>
+      decrease value
+    </button>
     </>
   )
 }
