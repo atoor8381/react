db605d81d2888a496af6f84541053b0d9394ff01
atoor0807 <atoor0807@gmail.com>
2026-06-01 03:09:16 +0500

    Set up the github page uses the loader function

--- Reactrouter/src/main.jsx
+++ Reactrouter/src/main.jsx
@@ -3,30 +3,48 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Layout from './layout.jsx'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements,Route } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import {Github, loaderfunction} from './components/Github/Github.jsx'
 
-const router = createBrowserRouter([
-  {
-    path : "/",
-    element : <Layout />,
-    children : [
-      {
-        path : "",
-        element : <Home /> 
-      },
-      {
-        path: "/About",
-        element : <About />
-      }
-    ]
-  }
-])
+
+// const router = createBrowserRouter([
+//   {
+//     path : "/",
+//     element : <Layout />,
+//     children : [
+//       {
+//         path : "",
+//         element : <Home /> 
+//       },
+//       {
+//         path: "/About",
+//         element : <About />
+//       }
+//     ]
+//   }
+// ])
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<Layout />} >
+      <Route path = '' element = {<Home />} />
+      <Route path = "/About" element = {<About />} />
+      <Route
+      loader = {loaderfunction}
+      path = "/Github" element = {<Github />} />
+    </Route>
+  )
+)
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <RouterProvider router = {router} />
+    {/* The RouterProvider component is used to provide the routing system to the application */}
   </StrictMode>,
 )
